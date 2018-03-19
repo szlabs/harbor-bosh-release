72813dbc85e700cbf33394f5d1502a69951c73f1
olivier <olivier.beyler@orange.com>
2018-03-19 13:24:09 +0000

    migrate to 1.4.0 harbor version

--- scripts/download_blobs.sh
+++ scripts/download_blobs.sh
@@ -8,8 +8,7 @@ cd $files_dir
 wget https://download.docker.com/linux/static/stable/x86_64/docker-17.06.2-ce.tgz
 wget -O docker-compose-Linux-x86_64-1.16.1 https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64
 wget https://www.openssl.org/source/openssl-1.0.2l.tar.gz
-wget -O harbor-offline-installer-latest.tgz https://github.com/vmware/harbor/releases/download/v1.2.0/harbor-offline-installer-v1.2.0.tgz
-
+wget -O harbor-offline-installer-latest.tgz https://storage.googleapis.com/harbor-releases/release-1.4.0/harbor-offline-installer-v1.4.0.tgz
 cd -
 cd ..
 bosh add-blob $files_dir/docker-17.06.2-ce.tgz docker/docker-17.06.2-ce.tgz
